bc1dae2ede46ba8cad73027dc35896048f8714b7
Aarij khan <aarij0100@gmail.com>
2024-10-30 18:06:59 +0500

    new commit

--- app/login.jsx
+++ app/login.jsx
@@ -1,11 +1,20 @@
-import { View, Text, StyleSheet, TextInput, ActivityIndicator } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  ActivityIndicator,
+} from "react-native";
 import React, { useState } from "react";
 import { TouchableOpacity } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { auth } from "./firebase.config/firebase";
 import { router } from "expo-router";
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
-import Toast from 'react-native-toast-message';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import Toast from "react-native-toast-message";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -22,125 +31,118 @@ const Login = () => {
           setLoading(false);
           setEmail("");
           setPassword("");
-            router.push("main");
+          router.push("main");
         })
         .catch((error) => {
           alert(error.message);
           setLoading(false);
-          
         });
-    }else{
-        Toast.show({
-            type: 'error',
-            text1: 'Something went wrong!',
-            text2: 'Please fill all the fields'
-          });
+    } else {
+      Toast.show({
+        type: "error",
+        text1: "Something went wrong!",
+        text2: "Please fill all the fields",
+      });
     }
   };
   return (
     <>
-    <Toast />
-    
-   
-    <View style={styles.container}>
-      <View style={styles.textView}>
-      <Text style={styles.text}>Welcome</Text>
-      <Text style={styles.text}>Back!</Text>
+      <View style={styles.container}>
+        <View style={styles.textView}>
+          <Text style={styles.text}>Welcome</Text>
+          <Text style={styles.text}>Back!</Text>
+          <Toast />
+        </View>
 
+        <View style={styles.main}>
+          <TextInput
+            placeholder="Enter email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+            style={styles.input}
+          />
+          <TextInput
+            placeholder="Enter password"
+            secureTextEntry={true}
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            style={styles.input}
+          />
+          <View style={styles.forgetContainer}>
+            <Text style={styles.forget}>Forgot password?</Text>
+          </View>
+          <TouchableOpacity onPress={handleLogin} style={styles.btn}>
+            {loading ? (
+              <ActivityIndicator size={50} color={white} />
+            ) : (
+              <Text style={styles.innerBtn}>Login</Text>
+            )}
+          </TouchableOpacity>
+        </View>
       </View>
-
- 
-    <View style={styles.main}>
-      <TextInput
-        placeholder="Enter email"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Enter password"
-        secureTextEntry={true}
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-        style={styles.input}
-      />
-      <View style={styles.forgetContainer}>
-      <Text style={styles.forget}>Forgot password?</Text>
-
-      </View>
-      <TouchableOpacity onPress={handleLogin} style={styles.btn}>
-        {loading ? (
-          <ActivityIndicator size={50} color={white} />
-        ) : (
-          <Text style={styles.innerBtn}>Login</Text>
-        )}
-      </TouchableOpacity>
-      
-    </View>
-    </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
   },
-  forgetContainer:{
-    width:wp('80%'),
-    justifyContent:'flex-end',
-    alignItems:'flex-end'
+  forgetContainer: {
+    width: wp("80%"),
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
   },
-  innerBtn:{
-    fontSize:20,
-    color:'white'
+  innerBtn: {
+    fontSize: 20,
+    color: "white",
   },
-  main:{
-    flex:1,
-    justifyContent:'flex-start',
-    alignItems:'center',
-    gap:15
+  main: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    gap: 15,
   },
-  input:{
-    height:hp('7%'),
-    width:wp('80%'),
-    borderWidth:1,
-    borderColor:'black',
-    borderRadius:10,
-    marginBottom:10,
-    padding:10,
-    fontSize:17,
-    color:'black'
+  input: {
+    height: hp("7%"),
+    width: wp("80%"),
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 10,
+    marginBottom: 10,
+    padding: 10,
+    fontSize: 17,
+    color: "black",
   },
-  text:{
-    fontSize:40,
-    fontWeight:'bold',
-    color:'black',
-    marginTop:5,
-    marginLeft:20
+  text: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "black",
+    marginTop: 5,
+    marginLeft: 20,
   },
-  textView:{
-    paddingHorizontal:20,
-    paddingVertical:30,
+  textView: {
+    paddingHorizontal: 20,
+    paddingVertical: 30,
   },
-  btn:{
-    height:hp('7%'),
-    width:wp('80%'),
-    borderWidth:1,
-    borderColor:'black',
-    borderRadius:10,
-    marginBottom:10,
-    padding:10,
-    backgroundColor:'#F83758',
-    justifyContent:'center',
-    alignItems:'center'
+  btn: {
+    height: hp("7%"),
+    width: wp("80%"),
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 10,
+    marginBottom: 10,
+    padding: 10,
+    backgroundColor: "#F83758",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  forget:{
-    fontSize:15,
-    fontWeight:'bold',
-    color:'black',
-    marginTop:5,
-    marginLeft:20
-  }
+  forget: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "black",
+    marginTop: 5,
+    marginLeft: 20,
+  },
 });
 export default Login;
